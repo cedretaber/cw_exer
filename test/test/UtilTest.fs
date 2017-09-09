7b556ca70c4fcddec0dedd6d44e339947e00022d
cedretaber <cedretaber@gmail.com>
2017-09-10 03:54:38 +0900

    Fix test up.

--- test/test/UtilTest.fs
+++ test/test/UtilTest.fs
@@ -12,11 +12,3 @@ module UtilTest =
     let ``引数を無視して、常に同じ値を返すこと`` (input : obj) =
       let value = "const value" in
       Assert.AreEqual (const' value input, value)
-
-    [<Test>]
-    let ``成功させるテスト`` () =
-      Assert.AreEqual (1, 1)
-
-    [<Test>]
-    let ``失敗させるテスト`` () =
-      Assert.AreEqual (1, 2)
