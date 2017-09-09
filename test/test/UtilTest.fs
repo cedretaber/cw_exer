857f1900bb02a1b34a5d59f5cd53144782dc4825
cedretaber <cedretaber@gmail.com>
2017-09-10 08:53:02 +0900

    Fix Util test.

--- test/test/UtilTest.fs
+++ test/test/UtilTest.fs
@@ -10,25 +10,33 @@ module UtilTest =
     [<Property>]
     let ``引数を無視して、常に同じ値を返すこと`` (input : obj) =
       let value = "const value" in
-      Assert.AreEqual (const' value input, value)
+      const' value input = value
   
   module is_true =
-    [<Property>]
-    let ``正しくBool値を返すこと`` (bool : bool) =
-      Assert.AreEqual (is_true bool, bool)
+    [<Test>]
+    let ``trueの場合`` () =
+      Assert.IsTrue (is_true true)
+
+    [<Test>]
+    let ``falseの場合`` () =
+      Assert.IsFalse (is_true false)
   
   module is_false =
-    [<Property>]
-    let ``正しくBool値を返すこと`` (bool : bool) =
-      Assert.AreEqual (is_false bool, not bool)
+    [<Test>]
+    let ``trueの場合`` () =
+      Assert.IsFalse (is_false true)
+
+    [<Test>]
+    let ``falseの場合`` () =
+      Assert.IsTrue (is_false false)
 
   module equals =
-    [<Property>]
+    [<Property(Verbose = true)>]
     let ``両者が等しい場合、trueを返すこと`` (obj : obj) =
-      Assert.IsTrue (equals obj obj)
+      equals obj obj
       
-    [<Property>]
+    [<Property(Verbose = true)>]
     let ``両者が異なる場合、falseを返すこと`` (obj1 : obj, obj2 : obj) =
       if obj1 = obj2
-      then Assert.IsTrue (equals obj1 obj2)
-      else Assert.IsFalse (equals obj1 obj2)
+      then equals obj1 obj2
+      else not <| equals obj1 obj2
