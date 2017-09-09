b1e12c65d57028357cb63f8f03ad40ce917a4bbb
cedretaber <cedretaber@gmail.com>
2017-09-09 23:32:46 +0900

    Add nugets and some spec.

---
+++ test/test/UtilTest.fs
@@ -0,0 +1,15 @@
+﻿namespace CardWirthEngine
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+
+module UtilTest =
+  open Util
+
+  module ``const'`` =
+
+    [<Property>]
+    let ``引数を無視して、常に同じ値を返すこと`` (input : obj) =
+      let value = "const value" in
+      const' value input = value
