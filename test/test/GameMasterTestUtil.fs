d022e2a5098f0436b74aaf7734550e2790cdd439
cedretaber <cedretaber@gmail.com>
2017-09-11 05:35:29 +0900

    Add test to add an adventurer.

--- test/test/GameMasterTestUtil.fs
+++ test/test/GameMasterTestUtil.fs
@@ -1,7 +1,10 @@
 ﻿namespace CardWirthEngineTest
 
 open CardWirthEngine.Data.Type
+open CardWirthEngine.Data.Casts
 open CardWirthEngine.Scenario.Info.Summary
+open CardWirthEngine.Cards
+open CardWirthEngine.Cards.Cast.Feature
 open CardWirthEngine.GameMasters.Cards
 open CardWirthEngine.GameMasters.State
 
@@ -16,6 +19,80 @@ module GameMasterTestUtil =
     , Adventurers.Nothing
     , Adventurers.Nothing
     , Adventurers.Nothing
+
+  let empty_cast : Cast.t =
+    { property =
+      { id = 1
+      ; name = "cast1"
+      ; image = empty_path
+      ; description = ""
+      ; level = 1
+      ; life =
+        { max = 1; current = 1 }
+      ; feature =
+        { no_effect =
+          { weapon = false; magic = false }
+        ; body_type =
+          { unholy       = false
+          ; undead       = false
+          ; automaton    = false
+          ; constructure = false
+          }
+        ; resist =
+          { ice = false; fire = false }
+        ; weakness =
+          { ice = false; fire = false }
+        }
+      ; ability = 
+        { physical =
+          { intelligence = 6
+          ; vitality     = 6
+          ; dexterity    = 6
+          ; mind         = 6
+          ; agility      = 6
+          ; strength     = 6
+          }
+        ; mental =
+          { aggressive = 0
+          ; cautious   = 0
+          ; cheerful   = 0
+          ; brave      = 0
+          ; trickish   = 0
+          }
+        ; enhance =
+          { action  = 0
+          ; avoid   = 0
+          ; resist  = 0
+          ; defense = 0
+          }
+        }
+      ; status =
+        { mentality =
+          { duration = 0
+          ; mentality = Mentality.Normal
+          }
+        ; paralyze   = 0
+        ; poison     = 0
+        ; bind       = 0
+        ; silence    = 0
+        ; face_up    = 0
+        ; anti_magic = 0
+        }
+      ; enhance =
+        { action  = 0
+        ; avoid   = 0 
+        ; resist  = 0
+        ; defense = 0
+        }
+      ; coupons = []
+      }
+    ; skill = []
+    ; item  = []
+    ; beast = []
+    }
+
+  let one_adventurer : Adventurers.t =
+    Adventurers.add empty_cast no_adventurers
 
   let empty_scenario : Scenario =
     { summary =
@@ -35,19 +112,19 @@ module GameMasterTestUtil =
       ; steps = Map.empty
       }
     ; cards =
-      { casts = Map.empty
+      { casts  = Map.empty
       ; skills = Map.empty
-      ; items = Map.empty
+      ; items  = Map.empty
       ; beasts = Map.empty
-      ; infos = Map.empty
+      ; infos  = Map.empty
       }
-    ; current_area = Area 0
+    ; current_area = Area 1
     ; global_state =
       { flags = Map.empty
       ; steps = Map.empty
       }
     ; eventStack = []
     ; selected = SelectedCast.None
-    ; companions = no_adventurers
+    ; companions = one_adventurer // 冒険者は1人
     ; bgm = Bgm.Stop
     }
---
+++ test/test/GameMasters/Cards/AdventurersTest.fs
@@ -0,0 +1,34 @@
+﻿namespace CardWirthEngineTest.Cards
+
+open NUnit.Framework
+
+open CardWirthEngineTest.TestUtils
+open CardWirthEngineTest.GameMasterTestUtil
+
+module AdventurersTest =
+  open CardWirthEngine.GameMasters.Cards.Adventurers
+
+  module add =
+
+    let empty_adv = Exist empty_cast in
+
+    [<Test>]
+    let ``空のパーティの冒険者を追加した場合`` () =
+      let one_adv =
+        empty_adv, Nothing, Nothing, Nothing, Nothing, Nothing in
+      add empty_cast no_adventurers === one_adv
+
+    [<Test>]
+    let ``既に冒険者がいるパーティに冒険者を追加した場合`` () =
+      let two_advs =
+        empty_adv, empty_adv, Nothing, Nothing, Nothing, Nothing in
+      add empty_cast one_adventurer === two_advs
+
+    [<Test>]
+    let ``冒険者が6人の場合、元のパーティがそのまま返ること`` () =
+      let last_adv =
+        { empty_cast with property = { empty_cast.property with  name = "last adventurer"  } } in
+      let advs =
+        empty_adv, empty_adv, empty_adv, empty_adv, empty_adv, Exist last_adv in
+      let _, _, _, _, _, result = add empty_cast advs in
+      result === Exist last_adv
