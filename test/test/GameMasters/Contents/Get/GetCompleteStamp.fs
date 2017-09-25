cbe364ac3b34792667951eb186b09d64e9be8577
cedretaber <cedretaber@gmail.com>
2017-09-25 13:54:18 +0900

    Add a test.

---
+++ test/test/GameMasters/Contents/Get/GetCompleteStamp.fs
@@ -0,0 +1,26 @@
+﻿namespace CardWirthEngineTest.GameMasters.Contents.Get
+
+module CompleteStamp =
+  open Expecto
+  open CardWirthEngineTest.GameMasterTestUtil
+
+  open CardWirthEngine.Data.Type
+  open CardWirthEngine.Cards
+  open CardWirthEngine.Scenario.Events.Content
+  open CardWirthEngine.GameMasters
+  open CardWirthEngine.GameMasters.Scenario
+  open CardWirthEngine.GameMasters.State
+  open CardWirthEngine.GameMaster
+  open CardWirthEngine.GameMasters.Cards
+
+  [<Tests>]
+  let get_completed_stamp =
+    testList "CardWirthEngine.Scenario.Events.Content.GetCompleteStamp" [
+      test "済み印を追加した場合" {
+        let scenario_name = "scenario1" in
+        let contents = GetCompleteStamp ([], scenario_name) in
+        let state = State.Scenario (empty_scenario, minimal_party, empty_global_data, state_random) in
+        let state', _ = read state [Content (empty_event, contents)] Input.None in
+        Expect.contains (state'.global_data.completed_scenarii) scenario_name "正しくシナリオ名が追加されていること"
+      }
+    ]
