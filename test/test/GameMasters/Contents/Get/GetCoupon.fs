4b5eea617f45fad3c96b7700b6f2a54df4aaa1f4
cedretaber <cedretaber@gmail.com>
2017-09-29 01:24:23 +0900

    Add a test.

--- test/test/GameMasters/Contents/Get/GetCoupon.fs
+++ test/test/GameMasters/Contents/Get/GetCoupon.fs
@@ -1,29 +1,28 @@
-﻿namespace CardWirthEngineTest.GameMasters.Contents.Get
+﻿module CardWirthEngineTest.GameMasters.Contents.Get.Coupon
 
-module Coupon =
-  open Expecto
-  open CardWirthEngineTest.GameMasterTestUtil
+open Expecto
+open CardWirthEngineTest.GameMasterTestUtil
 
-  open CardWirthEngine.Data.Type
-  open CardWirthEngine.Cards
-  open CardWirthEngine.Scenario.Events.Content
-  open CardWirthEngine.GameMasters
-  open CardWirthEngine.GameMasters.Scenario
-  open CardWirthEngine.GameMasters.State
-  open CardWirthEngine.GameMaster
-  open CardWirthEngine.GameMasters.Cards
+open CardWirthEngine.Data.Type
+open CardWirthEngine.Cards
+open CardWirthEngine.Scenario.Events.Content
+open CardWirthEngine.GameMasters
+open CardWirthEngine.GameMasters.Scenario
+open CardWirthEngine.GameMasters.State
+open CardWirthEngine.GameMaster
+open CardWirthEngine.GameMasters.Cards
 
-  [<Tests>]
-  let get_coupon =
-    testList "CardWirthEngine.Scenario.Events.Content.GetCoupon" [
-      test "選択中のキャストにクーポンを追加した場合" {
-        let coupon = "coupon1" in
-        let contents = GetCoupon ([], Target.Selected, 0, coupon) in
-        let scenario = { empty_scenario with selected = Scenario.PC Adventurers.First } in
-        let state = State.Scenario (scenario, minimal_party, empty_global_data, state_random) in
-        let state', output = read state [Content (empty_event, contents)] Input.None in
-        let (Some adv) = state'.selected_cast in
-        Expect.isTrue (Cast.has_coupon coupon adv) "正しくクーポンが追加されていること"
-        Expect.equal output Output.Coupon "正しくクーポン変更の通知が返ること"
-      }
-    ]
+[<Tests>]
+let get_coupon =
+  testList "CardWirthEngine.Scenario.Events.Content.GetCoupon" [
+    test "選択中のキャストにクーポンを追加した場合" {
+      let coupon = "coupon1" in
+      let contents = GetCoupon ([], Target.Selected, 0, coupon) in
+      let scenario = { empty_scenario with selected = Scenario.PC Adventurers.First } in
+      let state = State.Scenario (scenario, minimal_party, empty_global_data, state_random) in
+      let state', output = read state [Content (empty_event, contents)] Input.None in
+      let (Some adv) = state'.selected_cast in
+      Expect.isTrue (Cast.has_coupon coupon adv) "正しくクーポンが追加されていること"
+      Expect.equal output Output.Coupon "正しくクーポン変更の通知が返ること"
+    }
+  ]
---
+++ test/test/GameMasters/Contents/Lose/LoseCoupon.fs
@@ -0,0 +1,34 @@
+﻿module CardWirthEngineTest.GameMasters.Contents.Lose.Coupon
+
+open Expecto
+open CardWirthEngineTest.GameMasterTestUtil
+
+open CardWirthEngine.Data.Casts
+open CardWirthEngine.Data.Type
+open CardWirthEngine.Cards
+open CardWirthEngine.Scenario.Events.Content
+open CardWirthEngine.GameMasters
+open CardWirthEngine.GameMasters.Scenario
+open CardWirthEngine.GameMasters.State
+open CardWirthEngine.GameMaster
+open CardWirthEngine.GameMasters.Cards
+
+[<Tests>]
+let lose_coupon =
+  testList "CardWirthEngine.Scenario.Events.Content.LoseCoupon" [
+    test "選択中のキャストからクーポンを除去した場合" {
+      let coupon = "coupon1" in
+      let coupon_set =
+        CouponSet.add { name = coupon; value = 0 } CouponSet.empty in
+      let cast =
+        { empty_cast with property = { empty_cast.property with coupons = coupon_set } } in
+      let contents = LoseCoupon ([], Target.Selected, coupon) in
+      let scenario = { empty_scenario with selected = Scenario.PC Adventurers.First } in
+      let party = { minimal_party with adventurers = [|Adventurers.Exist cast|] } in
+      let state = State.Scenario (scenario, party, empty_global_data, state_random) in
+      let state', output = read state [Content (empty_event, contents)] Input.None in
+      let (Some adv) = state'.selected_cast in
+      Expect.isFalse (Cast.has_coupon coupon adv) "正しくクーポンが削除されていること"
+      Expect.equal output Output.Coupon "正しくクーポン変更の通知が返ること"
+    }
+  ]
