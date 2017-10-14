89cc105b345f7980c53b7d4f98086878ec2005d8
cedretaber <cedretaber@gmail.com>
2017-10-14 22:14:58 +0900

    Add tests.

--- test/test/GameMasters/Contents/Visual/LoseBgImage.fs
+++ test/test/GameMasters/Contents/Visual/LoseBgImage.fs
@@ -1,4 +1,4 @@
-﻿module CardWirthEngineTest.GameMasters.Contents.Visual.ReplaceBgImage
+﻿module CardWirthEngineTest.GameMasters.Contents.Visual.LoseBgImage
 
 open Expecto
 open CardWirthEngineTest.GameMasterTestUtil
@@ -15,11 +15,11 @@ open CardWirthEngine.Data.Type
 open CardWirthEngine.Scenario
 
 [<Tests>]
-let replace_bg_image =
+let lose_bg_image =
 
   let set_cellname = BackgroundImage.t.property_ >-> BackgroundImage.Property.cellname_ |> Optic.set
 
-  testList "CardWirthEngine.Scenario.Events.Content.ReplaceBgImage" [
+  testList "CardWirthEngine.Scenario.Events.Content.LoseBgImage" [
     let cellname = "cell1" in
     let image =
       BackgroundImage.BackgroundImage
@@ -33,48 +33,45 @@ let replace_bg_image =
           ; mask = false
           ; path = ""
           }
-        )
+        ) in
     let image0 = set_cellname (Some "cell0") image in
     let image2 = set_cellname (Some "cell2") image in
-    let images = Scenario.sort_backgrounds [image0; image; image2]
     let depiction : BackgroundImage.Depiction =
       { transition = Transition.None
       ; transition_speed = 5
       ; doanime = true
       ; ignore_effectbooster = false
-      }
-
-    yield test "置換対象の背景が存在しない場合" {
+      } in
+    
+    yield test "対象の背景が1枚の場合" {
+      let images = Scenario.sort_backgrounds [image0; image; image2] in
       let scenario = Scenario.set_backgrounds images empty_scenario in
       let state = State.Scenario (scenario, minimal_party, empty_global_data, state_random) in
-      let r_image1 = image |> (fun (p : BackgroundImage.Property ) -> { p with cellname = Some "replaced cell 1" }) ^% BackgroundImage.t.property_ in
-      let r_image2 = image |> (fun (p : BackgroundImage.Property ) -> { p with cellname = Some "replaced cell 2" }) ^% BackgroundImage.t.property_ in
-      let contents = ReplaceBgImage ([], depiction, "no such name", [r_image1; r_image2])
+      let contents = LoseBgImage ([], depiction, cellname) in
       let state', output = read state [Content (empty_event, contents)] Input.None in
-      let expected = images
+      let expected = [image0; image2]
       Expect.equal (State.get_backgrounds state') (Some expected) "正しく置換されること"
       Expect.equal output (Output.ChangeBackground (expected, depiction)) "正しく置換後の背景が返ること"
     }
-
-    yield test "置換対象の背景が1つだけある場合" {
+    
+    yield test "対象の背景が無い場合" {
+      let images = Scenario.sort_backgrounds [image0; image2] in
       let scenario = Scenario.set_backgrounds images empty_scenario in
       let state = State.Scenario (scenario, minimal_party, empty_global_data, state_random) in
-      let r_image = image |> (fun (p : BackgroundImage.Property ) -> { p with cellname = Some "replaced cell 1" }) ^% BackgroundImage.t.property_ in
-      let contents = ReplaceBgImage ([], depiction, cellname, [r_image])
+      let contents = LoseBgImage ([], depiction, cellname) in
       let state', output = read state [Content (empty_event, contents)] Input.None in
-      let expected = Scenario.sort_backgrounds [image0; r_image; image2]
+      let expected = [image0; image2]
       Expect.equal (State.get_backgrounds state') (Some expected) "正しく置換されること"
       Expect.equal output (Output.ChangeBackground (expected, depiction)) "正しく置換後の背景が返ること"
     }
-
-    yield test "置換対象の背景が複数ある場合" {
+    
+    yield test "対象の背景が複数枚の場合" {
+      let images = Scenario.sort_backgrounds [image; image0; image; image2; image] in
       let scenario = Scenario.set_backgrounds images empty_scenario in
       let state = State.Scenario (scenario, minimal_party, empty_global_data, state_random) in
-      let r_image1 = image |> (fun (p : BackgroundImage.Property ) -> { p with cellname = Some "replaced cell 1" }) ^% BackgroundImage.t.property_ in
-      let r_image2 = image |> (fun (p : BackgroundImage.Property ) -> { p with cellname = Some "replaced cell 2" }) ^% BackgroundImage.t.property_ in
-      let contents = ReplaceBgImage ([], depiction, cellname, [r_image1; r_image2])
+      let contents = LoseBgImage ([], depiction, cellname) in
       let state', output = read state [Content (empty_event, contents)] Input.None in
-      let expected = Scenario.sort_backgrounds [image0; r_image1; r_image2; image2]
+      let expected = [image0; image2]
       Expect.equal (State.get_backgrounds state') (Some expected) "正しく置換されること"
       Expect.equal output (Output.ChangeBackground (expected, depiction)) "正しく置換後の背景が返ること"
     }
